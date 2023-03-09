8af23cd217c4403a3df29c0aa9b6576d6e0df5d9
Nico <nico.hincelin@gmail.com>
2023-03-09 23:49:21 +0100

    Initial commit

--- app.js
+++ app.js
@@ -3,45 +3,49 @@ const mongoose = require("mongoose");
 const Request = require("./models/request.model")
 const bodyParser = require('body-parser');
 const helmet = require("helmet")
+require('dotenv').config();
 
-const dev_db_url = "mongodb+srv://sbstats:adec594ea@cluster0.0rllv.mongodb.net/grobe_license?retryWrites=true&w=majority"
-const mongoDB = process.env.MongoDB || dev_db_url;
-
-console.log(process.env.MongoDB)
-
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true });
  
 
 const app = express();
 
-app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet())
 
 mongoose.connection.on("open", async function(ref) {
-   const data = await Request.findOne({pseudo: 'ronce'})
-   console.log(data);
-  });
+
+});
 
 app.post('/new_request', async (req, res) => {
     console.log(req.body);
-    const mac_address = req.body.mac_address
-    const data = await Request.findOne({'mac_address': mac_address})
+    const mac_address = req.body.mac_address;
+    const data = await Request.findOne({'mac_address': mac_address});
     if(!data) {
         const request = await Request.create({
             pseudo: req.body.pseudo,
             'mac_address': mac_address,
             status: 'WAITING'
         })
-        res.send(request)
-    } else res.send(data)
+        res.send(request);
+    } else res.send(data);
 })
 
-app.get('/get_license', async (req, res) => {
+app.get('/get_license', async (req, res, next) => {
     const mac_address = req.body.mac_address;
-    const id = req.body.id
+    const id = req.body.id;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        res.send(null);
+        return;
+      }
     const data = await Request.findOne({_id: id})
-    res.send(data)
+    if (!data) {
+        res.send(data);
+        return;
+    }
+    if (mac_address !== data.mac_address) data.status = "FORBIDEN";
+    res.send(data);
 })
 
 module.exports = app;
