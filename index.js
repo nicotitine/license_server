e90b6defd5ffd176bf9610abd0a8a15866a60c36
Nico <nico.hincelin@gmail.com>
2023-08-07 00:24:39 +0200

    Add time to log

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const errorHandler = error => {
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
+      error(bind + ' requires elevated privileges.');
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
+      error(bind + ' is already in use.');
       process.exit(1);
       break;
     default:
@@ -37,7 +37,7 @@ server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+  log('Listening on ' + bind);
 });
 
 
@@ -54,7 +54,7 @@ function findSocketBySocket(socket) {
 io.on('connection', (socket) => {
   
   socket.on('auth', async (ids) => {
-    console.log(ids.id + ' connected');
+    log(ids.id + ' connected');
     sockets.push({
       'id': ids.id,
       'remote_id': ids.remote_id,
@@ -64,7 +64,7 @@ io.on('connection', (socket) => {
     let pretented_remote = findSocketById(ids.remote_id)
     if (pretented_remote == undefined) return
     if (pretented_remote.remote_id == ids.id) {
-      console.log(pretented_remote.id + ' and ' + ids.id + ' handchecked')
+      log(pretented_remote.id + ' and ' + ids.id + ' handchecked')
       let remote_pseudo = await getPseudoFromId(pretented_remote.id)
       let pseudo = await getPseudoFromId(ids.id)
       pretented_remote.socket.emit('connexion_successful', pseudo)
@@ -75,7 +75,7 @@ io.on('connection', (socket) => {
   socket.on('update_remote_id', async (ids) => {
     let sock = findSocketById(ids.id)
     sock.remote_id = ids.remote_id
-    console.log(ids.id + ' remote_id updated to: ' + ids.remote_id)
+    log(ids.id + ' remote_id updated to: ' + ids.remote_id)
 
     let remote_sock = findSocketById(ids.remote_id)
 
@@ -91,72 +91,80 @@ io.on('connection', (socket) => {
   })
 
   socket.on('notify', (params) => {
-    console.log('notify received from ' + params.id);
+    log('notify received from ' + params.id);
     let sock = findSocketById(params.id)
     let remote_sock = findSocketById(sock.remote_id)
 
     if (remote_sock == undefined) socket.emit('wrong_remote_id', {'remote_id': sock.remote_id, 'request': 'notify'})
     else if (sock.id == remote_sock.remote_id) {
-      console.log('emit notify to ' + remote_sock.id)
+      log('emit notify to ' + remote_sock.id)
       remote_sock.socket.emit('notify', {'title': params.title, 'message': params.message})
     }
   }) 
 
   socket.on('ask_tp', (id) => {
-    console.log('ask_tp received from ' + id)
+    log('ask_tp received from ' + id)
     let sock = findSocketById(id)
     let remote_sock = findSocketById(sock.remote_id)
 
     if (remote_sock == undefined) socket.emit('wrong_remote_id', { 'remote_id': sock.remote_id, 'request': 'ask_tp' })
     else if (sock.id == remote_sock.remote_id) {
-      console.log('emit tp to ' + remote_sock.id)
+      log('emit tp to ' + remote_sock.id)
       remote_sock.socket.emit('tp')
     }
   })
 
   socket.on('ask_anti_afk', (id) => {
-    console.log('ask_anti_afk received from ' + id)
+    log('ask_anti_afk received from ' + id)
     let sock = findSocketById(id)
     let remote_sock = findSocketById(sock.remote_id)
 
     if (remote_sock == undefined) socket.emit('wrong_remote_id', { 'remote_id': sock.remote_id, 'request': 'ask_anti_afk' })
     else if (sock.id == remote_sock.remote_id) {
-      console.log('emit anti_afk to ' + remote_sock.id)
+      log('emit anti_afk to ' + remote_sock.id)
       remote_sock.socket.emit('anti_afk')
     }
   })
 
   socket.on('ask_switch', (id) => {
-    console.log('ask_switch received from ' + id)
+    log('ask_switch received from ' + id)
     let sock = findSocketById(id)
     let remote_sock = findSocketById(sock.remote_id)
 
     if (remote_sock == undefined) socket.emit('wrong_remote_id', { 'remote_id': sock.remote_id, 'request': 'ask_switch' })
     else if (sock.id == remote_sock.remote_id) {
-      console.log('emit switch to ' + remote_sock.id)
+      log('emit switch to ' + remote_sock.id)
       remote_sock.socket.emit('switch')
     }
   })
 
   socket.on('ask_screen', (id) => {
-    console.log('ask_screen received from ' + id)
+    log('ask_screen received from ' + id)
     sock = findSocketById(id)
     remote_sock = findSocketById(sock.remote_id)
 
     if (remote_sock == undefined) socket.emit('wrong_remote_id', {'remote_id': sock.remote_id, 'request': 'ask_screen'})
     else if (sock.id == remote_sock.remote_id) {
-      console.log('emit screen to ' + remote_sock.id)
+      log('emit screen to ' + remote_sock.id)
       remote_sock.socket.emit('screen')
     }
   })
 
   socket.on('disconnect', function () {
     let sock = findSocketBySocket(socket)
-    console.log(sock.id + ' disconnected. Socket count: ' + sockets.length);
+    log(sock.id + ' disconnected. Socket count: ' + sockets.length);
     let i = sockets.indexOf(sock);
     sockets.splice(i, 1);
   });
 
 });
+
+function log(text) {
+  console.log(new Date().toJSON() + '\t' + text)
+}
+
+function error(text) {
+  console.error(new Date().toJSON() + '\t' + text)
+}
 
 server.listen(port);
